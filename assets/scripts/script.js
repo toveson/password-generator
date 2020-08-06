dc2ff5e128054f7524ca1cc7cd0ef708492224ab
Trever O <toveson9522@gmail.com>
2020-08-05 19:34:44 -0600

    added psuedo code to .js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+    // code to be ran
+    console.log(generatePassword)
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +17,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// user clicks on a button
+
+
+// collect user input
+// *length
+    // *min 8 characters
+    // *max 128 characters
+// *symbols
+    // *array of symbols
+// *numbers
+    // *array of 0-9
+// *uppercase letters
+    // *pull array of lowercase numbers and change them to uppercase
+// *lowercase letters
+    // *array of a-z
+
+// randomize the input order
+
+// select random characters in each category
+// * random letters
+// * random symbols
+// * random numbers
+
+// display on screen
