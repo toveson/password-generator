e3e83ba91df7dc292759d92ccb461efa4295b069
Trever O <toveson9522@gmail.com>
2020-08-05 23:50:09 -0600

    added a check to make sure at least one type of character is selected

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,24 +2,34 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var passwordLength = prompt("How many characters would you like your password to be?")
+  var passwordLength = prompt("How many characters would you like your password to be?");
   console.log(passwordLength);
   
-  if (passwordLength > 8) {
-    var passwordSymbol = confirm("Would you like symbols in your password?")
+  if (passwordLength > 7 && passwordLength < 129) {
+    var passwordSymbol = confirm("Would you like symbols in your password?");
     console.log(passwordSymbol);
-    var passwordNumber = confirm("Would you like numbers in your password?")
+    var passwordNumber = confirm("Would you like numbers in your password?");
     console.log(passwordNumber);
-    var littleLetter = confirm("Would you like lowercase letters in your password?")
+    var littleLetter = confirm("Would you like lowercase letters in your password?");
     console.log(littleLetter);
-    var bigLetters = confirm("Would you like uppercase letters in your password?")
+    var bigLetters = confirm("Would you like uppercase letters in your password?");
     console.log(bigLetters);
   }
 
   else {
-    alert ("Password must be between 8 and 100 characters!")
+    alert ("Password must be between 8 and 128 characters!")
   }
+  // atleast one type of charater must be true, if not alert must choose atleast one character type
+  if (passwordSymbol || passwordNumber || littleLetter || bigLetters) {
+    alert("Your password will now be generated!")
+  }
+  
+  else {
+    alert("must choose atleast one character type")
+  }
+  
 }
+
 
 // console.log(generatePassword)
 
