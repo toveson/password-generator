34251e94e260c263b358bee660a0c27d581474c4
Trever O <toveson9522@gmail.com>
2020-08-07 20:00:51 -0600

    cleaned up arrays using .split

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -37,28 +37,17 @@ function generatePassword() {
 
 
   // *array of a-z in lowercase
-  let smallAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-  // let smallAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  // console.log("smallAlphabet length = " + smallAlphabet.length);
+  let smallAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 
   // *array of a-z in uppercase
-  let largeAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  // var largeAlphabet = smallAlphabet.lower
-  // console.log("largeAlphabet length = " + largeAlphabet.length);
-
-
+  let largeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
   // *array of symbols
-  let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "}", ";", ":", "<", ">", ",", ".", "?", "/", "|"];
-  // console.log("symbols length = " + symbols.length);
-
-
+  let symbols = "!@#$%^&*()_-+={[]}|\,.?/<>".split("");
 
   // *array of 0-9
-  let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  // console.log("numbers length = " + numbers.length);
+  let numbers = "0123456789".split("");
 
   // math.random and number.floor I think?
   // only from true charcter types
